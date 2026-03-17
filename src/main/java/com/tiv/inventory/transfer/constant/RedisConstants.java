fb20dcc2953563ac1c5a7f8faa38b3cc47d6e98e
tivvvv <18612553935@163.com>
2026-03-17 12:55:36 +0800

    feat: 重复消费幂等处理.

---
+++ src/main/java/com/tiv/inventory/transfer/constant/RedisConstants.java
@@ -0,0 +1,7 @@
+package com.tiv.inventory.transfer.constant;
+
+public interface RedisConstants {
+
+    String PRODUCT_SALE_KEY = "product_sale_key";
+
+}
--- src/main/java/com/tiv/inventory/transfer/consumer/AgentConsumer.java
+++ src/main/java/com/tiv/inventory/transfer/consumer/AgentConsumer.java
@@ -1,21 +1,25 @@
 package com.tiv.inventory.transfer.consumer;
 
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.IdUtil;
-import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.alibaba.cloud.ai.graph.CompiledGraph;
 import com.alibaba.cloud.ai.graph.OverAllState;
 import com.alibaba.cloud.ai.graph.RunnableConfig;
 import com.tiv.inventory.transfer.constant.Constants;
+import com.tiv.inventory.transfer.constant.RedisConstants;
 import com.tiv.inventory.transfer.constant.TopicConstants;
+import com.tiv.inventory.transfer.domain.request.ProductSaleRequest;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
@@ -24,6 +28,9 @@ public class AgentConsumer {
     @Resource
     private CompiledGraph compiledGraph;
 
+    @Resource
+    private RedisTemplate<String, String> redisTemplate;
+
     @KafkaListener(topics = {TopicConstants.PRODUCT_SALE_TOPIC}, groupId = "group-1")
     public void consume(List<String> messages, Acknowledgment acknowledgment) {
         log.info("AgentConsumer--consume--batch size: {}", messages.size());
@@ -31,11 +38,17 @@ public class AgentConsumer {
             for (String message : messages) {
                 log.info("AgentConsumer--consume--message: {}", message);
                 // 解析消息
-                JSONObject jsonObject = JSONUtil.parseObj(message);
-                Integer productId = jsonObject.getInt(Constants.PRODUCT_ID);
+                ProductSaleRequest request = JSONUtil.toBean(message, ProductSaleRequest.class);
+                String redisKey = String.format("%s:%s:%s:%s", RedisConstants.PRODUCT_SALE_KEY, request.getProductId(), request.getWarehouseId(), request.getSaleDate());
+                Boolean result = redisTemplate.opsForValue().setIfAbsent(redisKey, message, 1L, TimeUnit.DAYS);
+                if (BooleanUtil.isFalse(result)) {
+                    log.info("AgentConsumer--consume--message has been consumed, message: {}", message);
+                    acknowledgment.acknowledge();
+                    return;
+                }
                 String threadId = IdUtil.simpleUUID();
                 Map<String, Object> inputs = Map.of(
-                        Constants.PRODUCT_ID, productId,
+                        Constants.PRODUCT_ID, String.valueOf(request.getProductId()),
                         Constants.THREAD_ID, threadId);
                 RunnableConfig runnableConfig = RunnableConfig.builder()
                         .threadId(threadId)
